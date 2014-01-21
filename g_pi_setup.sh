94677abb59d8f8ecd5d81a0a6d68575a31b62bb1
Gregory S. Kielian <gregory.kielian@gmail.com>
2014-01-20 20:19:14 -0800

    Create g_pi_setup.sh

---
+++ g_pi_setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+#make sure we're up 2 date
+sudo apt-get update
+sudo apt-get upgrade
+
+
+#some serious stuff
+sudo apt-get install -y git
+sudo apt-get install -y vim
+
+
+#some python stuff
+sudo apt-get install -y python-pip python2.7-dev
+sudo apt-get install -y python-serial python3-serial
+
+
+#gpio
+sudo pip install -y RPi.GPIO
+
+#arduino
+sudo apt-get install -y arduino
+
+sudo usermod -a -G tty pi
+sudo usermod -a -G dialout pi
+
+
+
+#and some fun stuff
+sudo apt-get install -y espeak
+sudo apt-get install -y figlet
+sudo apt-get install -y cowsay
+
